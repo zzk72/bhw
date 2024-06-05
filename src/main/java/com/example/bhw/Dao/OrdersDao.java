e18cad1f70bfaf1c3768f9b57d48f57cdc71817f
zzk <zzk72@foxmail.com>
2024-06-05 11:31:05 +0800

    存档

--- src/main/java/com/example/bhw/Dao/OrdersDao.java
+++ src/main/java/com/example/bhw/Dao/OrdersDao.java
@@ -4,6 +4,8 @@ import com.example.bhw.Entity.Orders;
 import jakarta.ejb.Stateless;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Stateless
@@ -29,5 +31,21 @@ public class OrdersDao {
         return em.createQuery("SELECT COUNT(o) FROM Orders o WHERE o.viewpoint.id = :viewpointId AND o.status <> 'checked_in'", Long.class)
                 .setParameter("viewpointId", viewpointId)
                 .getSingleResult();
+    }
+
+    public void checkin(int orderId) {
+        Orders order = getOrder(orderId);
+        order.setStatus("checked");
+        em.merge(order);
+    }
+
+    public Orders getEarliestOrderAfterCurrentTime(int userId, String viewpointId, LocalDateTime currentTime) {
+        Orders order = em.createQuery("select o from Orders o where o.user.id = :userId and o.viewpoint.id = :viewpointId and o.reservationTime > :currentTime and o.status = 'unchecked' order by o.reservationTime", Orders.class)
+                .setParameter("userId", userId)
+                .setParameter("viewpointId", viewpointId)
+                .setParameter("currentTime", currentTime)
+                .setMaxResults(1)
+                .getSingleResult();
+        return order;
     }
 }
